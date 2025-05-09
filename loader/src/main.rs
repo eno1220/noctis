df562234c85cfe99b28a15a0cc7af52bc04937f4
eno1220 <eno1220dev@gmail.com>
2025-05-10 04:21:36 +0900

    feat: add root directory retrieval

--- loader/src/main.rs
+++ loader/src/main.rs
@@ -2,7 +2,10 @@
 
 use core::arch::asm;
 use r_efi::efi::{self, MemoryDescriptor};
-use std::os::uefi;
+use std::{
+    ffi::OsStr,
+    os::uefi::{self, ffi::OsStrExt},
+};
 
 #[allow(dead_code)]
 struct MemoryMap {
@@ -27,16 +30,14 @@ struct MemoryMapIterator<'a> {
     offset: usize,
 }
 
-impl <'a> Iterator for MemoryMapIterator<'a> {
+impl<'a> Iterator for MemoryMapIterator<'a> {
     type Item = &'a MemoryDescriptor;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.offset >= self.map.map_size {
             return None;
         }
-        let desc = unsafe {
-            &*(self.map.buffer.add(self.offset) as *const MemoryDescriptor)
-        };
+        let desc = unsafe { &*(self.map.buffer.add(self.offset) as *const MemoryDescriptor) };
         self.offset += self.map.desc_size;
         Some(desc)
     }
@@ -97,6 +98,33 @@ fn get_memory_map(bt: *const efi::BootServices) -> Result<MemoryMap, efi::Status
     })
 }
 
+#[allow(const_item_mutation)]
+fn get_root_dir(
+    bt: *const efi::BootServices,
+) -> Result<*mut efi::protocols::file::Protocol, efi::Status> {
+    let mut simple_file_system: *mut efi::protocols::simple_file_system::Protocol =
+        core::ptr::null_mut();
+    let status = unsafe {
+        ((*bt).locate_protocol)(
+            &mut efi::protocols::simple_file_system::PROTOCOL_GUID as *mut efi::Guid,
+            core::ptr::null_mut(),
+            &mut simple_file_system as *mut *mut efi::protocols::simple_file_system::Protocol
+                as *mut *mut core::ffi::c_void,
+        )
+    };
+    if status != efi::Status::SUCCESS {
+        return Err(status);
+    }
+
+    let mut root_dir: *mut efi::protocols::file::Protocol = core::ptr::null_mut();
+    let status = unsafe { ((*simple_file_system).open_volume)(simple_file_system, &mut root_dir) };
+    if status != efi::Status::SUCCESS {
+        return Err(status);
+    }
+    Ok(root_dir)
+}
+
+#[allow(const_item_mutation)]
 fn main() {
     //let st = uefi::env::system_table().as_ptr() as *const efi::SystemTable;
     let bt = uefi::env::boot_services().unwrap().as_ptr() as *const efi::BootServices;
@@ -106,7 +134,7 @@ fn main() {
     let memory_map = match get_memory_map(bt) {
         Ok(map) => map,
         Err(status) => {
-            println!("Failed to get memory map: {:?}", status);
+            println!("Failed to get memory map: {status:?}");
             return;
         }
     };
@@ -116,6 +144,51 @@ fn main() {
             desc.physical_start, desc.number_of_pages, desc.r#type
         );
     }
+
+    let root_dir = match get_root_dir(bt) {
+        Ok(dir) => dir,
+        Err(status) => {
+            println!("Failed to get root directory: {status:?}");
+            return;
+        }
+    };
+
+    let mut kernel_file: *mut efi::protocols::file::Protocol = core::ptr::null_mut();
+    let mut kernel_file_name: Vec<u16> = OsStr::new("kernel.elf")
+        .encode_wide()
+        .chain(Some(0)) // Null-terminate the string
+        .collect();
+    let status = unsafe {
+        ((*root_dir).open)(
+            root_dir,
+            &mut kernel_file as *mut *mut efi::protocols::file::Protocol,
+            kernel_file_name.as_mut_ptr() as *mut efi::Char16,
+            efi::protocols::file::MODE_READ,
+            0,
+        )
+    };
+    if status != efi::Status::SUCCESS {
+        println!("Failed to open kernel file: {status:?}");
+        return;
+    }
+
+    let file_info: *mut efi::protocols::file::Info =
+        [0u8; core::mem::size_of::<efi::protocols::file::Info>() + 1024].as_mut_ptr()
+            as *mut efi::protocols::file::Info;
+    let mut file_info_size = core::mem::size_of::<efi::protocols::file::Info>() + 1024;
+    let status = unsafe {
+        ((*kernel_file).get_info)(
+            kernel_file,
+            &mut efi::protocols::file::INFO_ID,
+            &mut file_info_size,
+            file_info as *mut core::ffi::c_void,
+        )
+    };
+    if status != efi::Status::SUCCESS {
+        println!("Failed to get file info: {status:?}");
+        return;
+    }
+    println!("File Size: {}", unsafe { (*file_info).file_size });
 
     loop {
         unsafe { asm!("hlt") };
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 mkdir -p mnt/EFI/BOOT/
 cp loader/target/x86_64-unknown-uefi/debug/loader.efi mnt/EFI/BOOT/BOOTX64.EFI
+cp kernel/target/x86_64-unknown-kernel/debug/kernel mnt/kernel.elf
 qemu-system-x86_64 -m 2048M -bios thirdparty/RELEASEX64_OVMF.fd -drive format=raw,file=fat:rw:mnt -serial mon:stdio -nographic -no-reboot
